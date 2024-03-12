ff6b63d8f30acee529970237d4f1a32d5e3d1978
smtlao <163104243+smtlao@users.noreply.github.com>
2024-03-12 15:56:22 +0800

    Add files via upload

---
+++ Hello.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Hello = () => {
+    return <div>
+        <h1>WithJSX Hallo</h1>
+    </div>
+}
+
---
+++ Welcome.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+class Welcome extends Component {
+    render(){
+        return <h1>Welcome home big dawg</h1>
+    }
+}
+
+export default Welcome
